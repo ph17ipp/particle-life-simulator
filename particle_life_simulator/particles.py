d052c9823b6d920610c8dd082e08913b7023db92
Philipp Tran <philipp.tran@study.hs-duesseldorf.de>
2026-02-16 23:07:02 +0100

    Fixing ruff linter, updating Particle class

--- particle_life_simulator/particles.py
+++ particle_life_simulator/particles.py
@@ -1,28 +1,17 @@
 import numpy as np
 
 from . import physics
-from .interactionmatrix import inter_matrix
 
 
 class Particles:
-    def __init__(self, n_particles: int, n_type: int, width, height, inter_matrix):
+    def __init__(self, n_particles: int, n_type: int, width, height):
         self.n_particles = n_particles
         self.n_type = np.random.randint(0, n_type, n_particles)
         self.width = width
         self.height = height
         self.position = np.random.rand(n_particles, 2) * [width, height] 
         self.speed = np.zeros((n_particles, 2))
-        self.inter_matrix = inter_matrix 
-    '''  
-    def update_position(self):
-        self.position = np.random.normal(self.position, 5)
-        self.position[:, 0] = self.position[:, 0] % self.height
-        self.position[:, 1] = self.position[:, 1] % self.width
-        return self.position
-    '''
-
-    def set_interaction_matrix(self, matrix):
-        self.interaction_matrix = matrix
+        self.inter_matrix = np.zeros((n_type, n_type), dtype=float)
 
     def update_position(self, dt = 0.1, random_motion = 0.1):
         forces = physics.calculate_all_forces(self.position, self.n_type, self.n_particles, self.inter_matrix)
--- particle_life_simulator/simulation.py
+++ particle_life_simulator/simulation.py
@@ -2,7 +2,6 @@ import pygame
 import pygame_widgets
 from pygame_widgets.slider import Slider
 from pygame_widgets.textbox import TextBox
-import numpy as np
 
 from . import particles
 
@@ -35,6 +34,9 @@ def main():
     WIDTH = 1280
     HEIGHT = 720
 
+    # Creating particle objects
+    particle_object = particles.Particles(NUM_PARTICLES, NUM_TYPE, WIDTH, HEIGHT)
+
     # Initialize Pygame
     pygame.init()
 
@@ -61,12 +63,6 @@ def main():
             combinations.append((i, j))
 
     num_sliders = len(combinations)
-    
-    #interaction_matrix
-    MATRIX = interaction_matrix = np.zeros((NUM_TYPE, NUM_TYPE), dtype=float)
-
-    # Creating particle objects
-    particle_object = particles.Particles(NUM_PARTICLES, NUM_TYPE, WIDTH, HEIGHT, MATRIX)
 
     # Slider Layout
     left_margin = 40
@@ -157,10 +153,9 @@ def main():
         for (i, j), slider in zip(combinations, sliders):
             value = slider.getValue()
 
-            interaction_matrix[i, j] = value
-            interaction_matrix[j, i] = value
+            particle_object.inter_matrix[i, j] = value
+            particle_object.inter_matrix[j, i] = value
         
-        particle_object.set_interaction_matrix(interaction_matrix)
         particle_object.update_position(dt = 2)
         
         # Draw particles
